f508f31ae0a072504e3d873015511e63d0b865eb
lemmes33 <vinlemuel021@gmail.com>
2024-03-23 11:32:50 +0300

    division using arrow function index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// Function expression for divide
+function divide(a, b) {
+    return a / b;
+  }
+  // Arrow function for square
+const square = (x) => x * x;
+// Arrow function for add
+const add = (x, y) => x + y;
